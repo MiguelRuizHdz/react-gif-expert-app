00be51fe92e1f2a1029693e31229c41535895749
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-03-21 20:48:36 -0600

    Testing - Probando la aplicación

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,30 +1,36 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
-export const AddCategory = ( {setCategories} ) => {
+export const AddCategory = ( { setCategories } ) => {
 
     const [inputValue, setInputValue] = useState(''); // ''
-    
-    const handleInputChange =  (e) => setInputValue(e.target.value);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+        console.log('handleInputChange llamado');
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if ( inputValue.trim().length > 2 ){
-            setCategories( cats => [ inputValue, ...cats  ]); 
+        console.log('handleSubmit', inputValue );
+
+        if ( inputValue.trim().length > 2 ) {
+            setCategories(cats => [inputValue, ...cats]);
             setInputValue('');
         }
 
     }
 
-    return (
-            <form onSubmit={ handleSubmit }>
-                <input
-                    type="text"
-                    value={ inputValue }
-                    onChange={ handleInputChange  }
-                />
-            </form>
+    return ( 
+        <form onSubmit={ handleSubmit }>
+            <p> {inputValue} </p>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+        </form>
     )
 }
 
